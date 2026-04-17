e45be664e243a9770617f77916e94180afb29edb
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-17 20:21:20 +0800

    Add files via upload

---
+++ assets/useYuEBao-CtbyW6CH.js
@@ -0,0 +1,1 @@
+import{v as e,r as o,w as s}from"./index-CLrT31Vy.js";const d=e("YuEBao",()=>{const t=o(void 0);return{data:s({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{d as u};
